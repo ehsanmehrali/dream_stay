12f9d048484586ed0bfe44b49747790486511ee2
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-06-13 17:40:21 +0200

    Imported pkg(dotenv) was added to requirements.txt.

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = os.getenv('DEBUG', 'False') == 'True'
