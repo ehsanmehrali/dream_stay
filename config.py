dce6ddcbe7f59fc2a6acecaef75b19333e46765a
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-06-13 17:44:32 +0200

    The .env file was created.

--- config.py
+++ config.py
@@ -2,10 +2,4 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    DEBUG = os.getenv('DEBUG', 'False') == 'True'
