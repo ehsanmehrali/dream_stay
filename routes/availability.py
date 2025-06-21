c32cd7000589ad22bba1c4c1083da18089b64acc
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-06-21 20:41:58 +0200

    Remove An unused variable in add_availability.

--- routes/availability.py
+++ routes/availability.py
@@ -70,7 +70,6 @@ def add_availability():
         if not prop:
             return jsonify({'error': 'Property not found or not owned by user'}), 403
 
-        today = date.today()
         valid_items, input_dates = parse_valid_dates(dates_dict)
 
         # Query for check the existing dates in DB
@@ -109,43 +108,6 @@ def add_availability():
         return jsonify(results), 201
 
 
-
-@availability_bp.route('/availability/<int:availability_id>', methods=['PUT'])
-@jwt_required()
-def update_availability(availability_id):
-    """ Edit price and availability. """
-    data = request.get_json()
-    user_id = get_jwt_identity()
-
-    with get_db() as db:
-        user = db.query(User).get(user_id)
-
-        # Check availability_id
-        availability = db.query(Availability).get(availability_id)
-        if not availability:
-            return jsonify({'error': 'Availability not found'}), 404
-
-        # Host Identity verification
-        prop = db.query(Property).get(availability.property_id)
-        if not prop or prop.host_id != user.id:
-            return jsonify({'error': 'Unauthorized access to this availability'}), 403
-
-        if 'is_available' in data:
-            if not isinstance(data['is_available'], bool):
-                return jsonify({'error': 'is_available must be a boolean value'}), 400
-            if availability.is_reserved:
-                return jsonify({'error': 'Cannot change availability of a reserved date.'}), 400
-            availability.is_available = data['is_available']
-
-        if 'price' in data:
-            try:
-                availability.price = float(data['price'])
-            except ValueError:
-                return jsonify({'error': 'Invalid price format'}), 400
-
-        db.commit()
-
-        return jsonify({'msg': 'Availability updated'}), 200
 
 
 @availability_bp.route('/availability/property/<int:property_id>', methods=['GET'])
