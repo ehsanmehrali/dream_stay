ad21e4a14fcfade22c8f67780849a90fc390a7d1
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-06-25 11:26:08 +0200

    Refactored search route.

--- routes/search.py
+++ routes/search.py
@@ -1,24 +1,34 @@
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from flask import request, jsonify
 
 from flask import Blueprint
 
-from models import Property, Availability
+from models import Property
 from database import get_db
-
+from utils.availability import check_property_availability
 
 search_bp = Blueprint('search', __name__)
 
 
 @search_bp.route('/search', methods=['GET'])
 def search_properties():
+    """
+    Searches for bookable accommodations within a specific time
+    period based on location or accommodation title.
+    Returns a list of properties list:
+    [['property_id', 'title', 'location', 'price_per_night', 'total_price', 'available_from', 'available_to', 'total_night']]
+    """
     location = request.args.get('location')
+    title = request.args.get('title')
     check_in_str = request.args.get('check_in')
     check_out_str = request.args.get('check_out')
 
-    if not location or not check_in_str or not check_out_str:
-        return jsonify({'error': 'location, check_in, and check_out are required'}), 400
+    if not check_in_str or not check_out_str:
+        return jsonify({'error': 'check_in and check_out are required'}), 400
+
+    if not location and not title:
+        return jsonify({'error': 'location or title must be provided'}), 400
 
     try:
         check_in = datetime.strptime(check_in_str, "%Y-%m-%d").date()
@@ -30,24 +40,26 @@ def search_properties():
         return jsonify({'error': 'check_out must be after check_in'}), 400
 
     total_nights = (check_out - check_in).days
-    # Create a list of all stay dates
-    date_range = [check_in + timedelta(days=i) for i in range(total_nights)]
 
     with get_db() as db:
-        # Getting all properties in target location
-        properties = db.query(Property).filter_by(location=location, is_approved=True).all()
-        results = []
-
-        for prop in properties:
-            availability_records = db.query(Availability).filter(
-                Availability.property_id == prop.id,
-                Availability.date.in_(date_range),
-                Availability.is_available == True,
-                Availability.is_reserved == False,
-                Availability.is_blocked == False
+        # Getting all properties or exact property availability data, based on location or title
+        if title:
+            properties = db.query(Property).filter(
+                Property.title.ilike(f'%{title}%'),
+                Property.is_approved == True
+            ).all()
+        else:
+            properties = db.query(Property).filter_by(
+                location=location,
+                is_approved=True
             ).all()
 
-            if len(availability_records) == total_nights:
+        results = []
+        for prop in properties:
+            # using helper function from /utils
+            success, availability_records, _ = check_property_availability(db, prop.id, check_in, check_out)
+
+            if success:
                 total_price = sum([a.price for a in availability_records])
                 results.append({
                     'property_id': prop.id,
@@ -56,7 +68,7 @@ def search_properties():
                     'price_per_night': round(total_price / total_nights, 2),
                     'total_price': float(total_price),
                     'available_from': check_in_str,
-                    'available_to': check_out_str
+                    'available_to': check_out_str,
+                    'total_night': total_nights
                 })
-
         return jsonify(results), 200
