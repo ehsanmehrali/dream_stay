c428f0815818b80a5392b560292f9c72f56ff43a
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-09-28 13:32:46 +0200

    Add PropertyImage class.

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 
 
@@ -11,3 +10,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+    # Images
+    IMAGE_UPLOAD_DIR = os.getenv('IMAGE_UPLOAD_DIR', os.path.abspath('uploads'))
+    IMAGE_BASE_URL = os.getenv('IMAGE_BASE_URL', '/uploads')
+    IMAGE_MAX_MB = float(os.getenv('IMAGE_MAX_MB', '10'))
+    IMAGE_MAX_COUNT = int(os.getenv('IMAGE_MAX_COUNT', '20'))
+    IMAGE_ALLOWED_FORMATS = set((os.getenv('IMAGE_ALLOWED_FORMATS', 'jpg,jpeg,png,webp,heic')).lower().split(','))
